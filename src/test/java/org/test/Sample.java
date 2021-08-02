99711a100ad0ce22ef3b52b6fd5daea4da74c95f
SekarMano14 <sekarmano95@gmail.com>
2021-08-02 14:49:22 +0530

    Mano Code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -4,6 +4,8 @@ import org.base.BaseClass;
 
 public class Sample extends BaseClass {
 	public static void main(String[] args) {
+		System.out.println("Test1");
+		System.out.println("Test2");
 
 	}
 
